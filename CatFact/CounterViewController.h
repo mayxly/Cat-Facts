7e68bd099eec879cf23948170cb61b19ba1ed3d1
TribalScale <tribalscale@TribalScalesMBP.hitronhub.home>
2023-05-24 13:59:13 -0400

    Linked stepper and label to controller

--- CatFact/CounterViewController.h
+++ CatFact/CounterViewController.h
@@ -4,5 +4,9 @@
 
 @interface CounterViewController : UIViewController
 
+@property NSInteger *catCount;
+@property (strong, nonatomic) IBOutlet UILabel *label;
+@property (strong, nonatomic) IBOutlet UIStepper *stepper;
+
 @end
 
--- CatFact/CounterViewController.m
+++ CatFact/CounterViewController.m
@@ -15,8 +15,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //NSLog(@"%@", self.stepper.value);
+    self.stepper.wraps = YES;
 
 }
+
+
+- (IBAction)valueDidChanged:(UIStepper *)sender {
+//Whenever the stepper value increase and decrease the sender.value fetch the curent value of stepper
+        NSUInteger value= sender.value;
+        self.label.text= [NSString stringWithFormat:@"%02lu",value];
+}
+
+
+
 
 
 @end
