22516b0166fe3e797408e92b04de24b5bb616de5
May <may.ly7303@gmail.com>
2023-05-26 16:11:20 -0400

    API fetch to cat pictures

--- CatFact/BreedViewController.swift
+++ CatFact/BreedViewController.swift
@@ -1,67 +1,164 @@
-//
-//  BreedViewController.swift
-//  CatFact
-//
-//  Created by TribalScale on 2023-05-25.
-//
-
 import Foundation
 import UIKit
 
 struct CatBreed: Codable {
-    let description: String
     let name: String
-    let countryCode: String
+    let id: String
 }
 
-//struct Statement: Codable {
-//   let name: String
-//}
+struct CatImage: Codable {
+    let url: String
+}
 
-class BreedViewController : UIViewController{
-//    NSURL *URL = [NSURL URLWithString:@"https://api.thecatapi.com/v1/breeds"];
-    let url = URL(string: "https://api.thecatapi.com/v1/breeds")!
-    let catBreeds = [String]()
+class BreedViewController : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var catPicker: UIPickerView!
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet weak var catName: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let responseArray = [String]()
+    var breeds = [String]()
+    var catBreeds = [CatBreed]()
+    var catImages = [CatImage]()
+    
+    var chosenRow = 0
+    
     let error = NSError(domain: "com.example.error", code: 0, userInfo: [NSLocalizedDescriptionKey: "message"])
-
     
     override func viewDidLoad() {
-        NetworkManager.performGETRequestArray(url) { responseArray, error in
-//            responseArray?[0]
-            
-            guard error != nil else {
-                print(error!)
-                
-                // Error state
-                return
-            }
-            
+        let urlBreed = URL(string: "https://api.thecatapi.com/v1/breeds")!
+        NetworkManager.performGETRequestArray(urlBreed) { [self] responseArray, error in
+//            guard error != nil else {
+//                print("ERROR:", error ?? "error")
+//                return
+//            }
             guard let responseArray else {
-                // Empty state
                 return
             }
-            
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             do {
-                // TODO: [Any] to Data type.
-                let catBreeds = try decoder.decode([CatBreed].self, from: Data())
-                print(catBreeds[0].name)
+                let data = try JSONSerialization.data(withJSONObject: responseArray)
+                catBreeds = try decoder.decode([CatBreed].self, from: data)
             } catch {
-                // Error state
-                // maybe a retry
                 print(error)
             }
-            
-            
-//                print("Array: ", responseArray ?? [])
-//                for cat in responseArray ?? [] {
-//                    print(cat.name)
-//                }
+//            print("Count", catBreeds.count)
+//            print(catBreeds)
+            for i in 0..<catBreeds.count {
+                breeds.append(catBreeds[i].name)
+//                print(i, breeds[i])
+            }
+            //Connect Data
+            DispatchQueue.main.async {
+                self.catPicker.delegate = self
+                self.catPicker.dataSource = self
+                self.collectionView.delegate = self
+                self.collectionView.dataSource = self
+            }
+        }
+        catPicker.isHidden = true
+    }
 
-            
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return breeds.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return breeds[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectButton.setTitle("Generating...", for: [])
+        catPicker.isHidden = true
+        
+        let selectedBreed = catBreeds[row]
+        print(selectedBreed)
+        let urlImages = URL(string: "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=\(selectedBreed.id)")
+        NetworkManager.performGETRequestArray(urlImages) { [self] responseArray
+            , error in
+            guard let responseArray else {
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                let data = try JSONSerialization.data(withJSONObject: responseArray)
+                catImages = try decoder.decode([CatImage].self, from: data)
+            } catch {
+                print(error)
+            }
+            print("Count", catImages.count)
+            for i in 0..<catImages.count {
+                print(i, catImages[i])
+            }
+        }
+        selectButton.setTitle("Select a Breed", for: [])
+        catName.text = selectedBreed.name
+        self.chosenRow = row
+        collectionView.reloadData()
+        
+    }
+    
+    @IBAction func selectPressed(_ sender: UIButton) {
+        if catPicker.isHidden {
+            catPicker.isHidden = false
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "custom", for: indexPath)
+        cell.backgroundColor = UIColor.black
+        
+        let imageview:UIImageView=UIImageView(frame: CGRectMake(0, 0, 178, 178))
+        
+//        print("curCount:", catImages.count)
+//        if (catImages.count != 0) {
+//            print("chosenRow:", chosenRow)
+//            print("url:", catImages[chosenRow].url)
+            guard let url = URL(string: "https://cdn2.thecatapi.com/images/7isAO4Cav.jpg") else {
+                return cell
+            }
+//            let data = try? Data(contentsOf: url)
+            var image = UIImage(named: "logo.png")
+            
+//            if let imageData = data {
+//                image = UIImage(data: imageData)
+//            }
+            imageview.image = image
+            cell.contentView.addSubview(imageview)
+            //        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSizeMake(178, 178)
+    }
+
 }
